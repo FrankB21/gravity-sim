26f0a01e9cc215fb0279945dba32594fe96361af
Franco Berni <47229844+FrankB21@users.noreply.github.com>
2020-06-26 22:47:13 -0300

    Add SFML library

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
 
+#include <SFML/Graphics.hpp>
+
 #include "spdlog/spdlog.h"
 
 int main()
 {
     auto logger = spdlog::default_logger();
     logger->info("Logger initialized!");
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape  shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
